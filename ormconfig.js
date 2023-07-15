fe58b061828f6365420f2c76289cd21328288104
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-15 19:08:32 +0700

    perbaikan module dan strukture file

--- ormconfig.js
+++
@@ -1,7 +0,0 @@
-const AdminUser = require('nestjs-admin').AdminUserEntity
-module.exports = {
-  /* ... */,
-  entities: [/* ... */, AdminUser],
-  // Alternatively:
-  // entities: [/* ... */, 'node_modules/nestjs-admin/**/*.entity.js'],
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { DefaultAdminModule } from 'nestjs-admin';
-// import { TypeOrmModule } from 'typeorm';
-import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DefaultAdminModule, CatsModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })

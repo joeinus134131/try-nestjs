792e03c6e56b7aa6cbd8b61b0ad95e197ebaaa13
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-17 17:36:43 +0700

    update baru

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -19,5 +19,5 @@ export class UsersService {
     
       async findOne(username: string): Promise<user | undefined> {
         return this.users.find(user => user.username === username);
-      }    
+      }
 }

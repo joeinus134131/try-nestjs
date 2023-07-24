0ef41f26ad4da684a5c7256757f16d25b1fe92da
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-24 07:58:31 +0700

    backend(api): upen connection to public API (nestjs test)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MiddlewareConsumer } from '@nestjs/common/interfaces/middleware';
+import { ApiKeyMiddleware } from './lib/middleware/api_key.middleware';
+import { SanitizeHtmlMiddleware } from './lib/middleware/sanitize_html.middleware';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -9,4 +12,17 @@ import { UsersModule } from './users/users.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer
+      .apply(ApiKeyMiddleware)
+      .forRoutes(
+        { path: 'users/register', method: RequestMethod.POST },
+        { path: 'auth/login', method: RequestMethod.POST },        
+      );
+    
+    consumer
+      .apply(SanitizeHtmlMiddleware)
+      .forRoutes('*')
+  }
+}
